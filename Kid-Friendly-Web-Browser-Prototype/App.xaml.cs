3daadc5bf92623b0a1394379b5b99fd82baca6c9
Jared Rooprai <roopraijared@gmail.com>
2015-10-25 20:26:39 -0600

    init commit

---
+++ Kid-Friendly-Web-Browser-Prototype/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Kid_Friendly_Web_Browser_Prototype
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
