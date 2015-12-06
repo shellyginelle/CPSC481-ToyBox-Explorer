0ee98bcef917ca40b2ec9ee83c751509d43e9106
shellyginelle <sgsicat@ucalgary.ca>
2015-12-06 13:45:15 -0700

    adding TimeOut after time complete

--- Kid-Friendly-Web-Browser-Prototype/MainWindow.xaml.cs
+++ Kid-Friendly-Web-Browser-Prototype/MainWindow.xaml.cs
@@ -25,11 +25,15 @@ namespace Kid_Friendly_Web_Browser_Prototype
         private void progressBarChange(object sender, EventArgs e)
         {
             // 1800 seconds = 30 minutes MAX TIME FOR WEB BROWSER 
-            for (int i = 0; i < 1800; i++)
+            for (int i = 0; i < 50; i++)
             {
                 pbStatus.Dispatcher.Invoke(() => pbStatus.Value = i, System.Windows.Threading.DispatcherPriority.Background);
-                Thread.Sleep(1800);
+                Thread.Sleep(50);
             }
+           
+            TimeOut popup = new TimeOut(); //Create new window to the exit page 
+            popup.Show();
+            Close();
         }
 
     }
