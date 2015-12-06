e20db5cff9556d5561ede6f1e0feb2916b91a28f
KeiGiang <kevin.giang@hotmail.com>
2015-12-05 18:08:05 -0700

    Category Grid User Control
    Added a user control to help display the different website icons. Tinted
    the UC for some visibility.

--- Kid-Friendly-Web-Browser-Prototype/MainWindow.xaml.cs
+++ Kid-Friendly-Web-Browser-Prototype/MainWindow.xaml.cs
@@ -37,12 +37,24 @@ namespace Kid_Friendly_Web_Browser_Prototype
         private void progressBarChange(object sender, EventArgs e)
         {
             // 1800 seconds = 30 minutes MAX TIME FOR WEB BROWSER 
-            for (int i = 0; i < 1800; i++)
+            for (int i = 0; i < 200; i++)
             {
                 pbStatus.Dispatcher.Invoke(() => pbStatus.Value = i, System.Windows.Threading.DispatcherPriority.Background);
-                Thread.Sleep(1800);
+                Thread.Sleep(200);
             }
         }
 
+        private void games_Click(object sender, RoutedEventArgs e)
+        {
+            CategoryGrid1 catGrid = new CategoryGrid1();
+            Window window = new Window
+            {
+                Title = "My User Control Dialog",
+                Content = new CategoryGrid1()
+            };
+
+            window.ShowDialog();
+
+        }
     }
 }
