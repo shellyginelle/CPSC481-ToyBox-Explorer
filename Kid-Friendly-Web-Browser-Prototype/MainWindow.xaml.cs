f03db9bf4bdec828951696122a549428de66bf5c
shellyginelle <sgsicat@ucalgary.ca>
2015-12-06 14:38:46 -0700

    adding Visibility to change between browser and home

--- Kid-Friendly-Web-Browser-Prototype/MainWindow.xaml.cs
+++ Kid-Friendly-Web-Browser-Prototype/MainWindow.xaml.cs
@@ -14,13 +14,54 @@ namespace Kid_Friendly_Web_Browser_Prototype
         public MainWindow()
         {
             InitializeComponent();
+            homeButton.Visibility = Visibility.Hidden;
         }
 
         private void goToWebsite(object sender, RoutedEventArgs e)
         {
-            Browser browserWindow = new Browser();
-            browserWindow.Show();
-            Close();
+            sesame.Visibility = Visibility.Hidden;
+            dora.Visibility = Visibility.Hidden;
+            turtles.Visibility = Visibility.Hidden;
+            trees.Visibility = Visibility.Hidden;
+
+            games.Visibility = Visibility.Hidden;
+            video.Visibility = Visibility.Hidden;
+            learn.Visibility = Visibility.Hidden;
+            fave.Visibility = Visibility.Hidden;
+
+            buttonRight.Visibility = Visibility.Hidden;
+            buttonLeft.Visibility = Visibility.Hidden;
+
+            search.Visibility = Visibility.Hidden;
+            settings.Visibility = Visibility.Hidden;
+
+            homeButton.Visibility = Visibility.Visible;
+
+            mainPanel.Children.Clear();
+            mainPanel.Children.Add(websiteControl);
+        }
+
+        private void goHome(object sender, RoutedEventArgs e)
+        {
+            sesame.Visibility = Visibility.Visible;
+            dora.Visibility = Visibility.Visible;
+            turtles.Visibility = Visibility.Visible;
+            trees.Visibility = Visibility.Visible;
+
+            games.Visibility = Visibility.Visible;
+            video.Visibility = Visibility.Visible;
+            learn.Visibility = Visibility.Visible;
+            fave.Visibility = Visibility.Visible;
+
+            buttonRight.Visibility = Visibility.Visible;
+            buttonLeft.Visibility = Visibility.Visible;
+
+            search.Visibility = Visibility.Visible;
+            settings.Visibility = Visibility.Visible;
+
+            homeButton.Visibility = Visibility.Hidden;
+
+            mainPanel.Children.Clear();
         }
 
         private void progressBarChange(object sender, EventArgs e)
