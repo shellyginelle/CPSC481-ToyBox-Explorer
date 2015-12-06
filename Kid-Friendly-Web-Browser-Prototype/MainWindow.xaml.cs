5f8e6c206ce6b23928a965d77641e4c73a3e9a3a
KeiGiang <kevin.giang@hotmail.com>
2015-12-06 13:21:04 -0700

    Arrows Cycle Thru Categories
    Arrows will now cycle thru the UC 1-3 and loop as well. Both arrows
    work.

--- Kid-Friendly-Web-Browser-Prototype/MainWindow.xaml.cs
+++ Kid-Friendly-Web-Browser-Prototype/MainWindow.xaml.cs
@@ -21,6 +21,10 @@ namespace Kid_Friendly_Web_Browser_Prototype
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool catGrid1 = false;
+        private bool catGrid2 = false;
+        private bool catGrid3 = false;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,7 +32,6 @@ namespace Kid_Friendly_Web_Browser_Prototype
 
         private void goToWebsite(object sender, RoutedEventArgs e)
         {
-
             Browser browserWindow = new Browser();
             browserWindow.Show();
             Close();
@@ -48,30 +51,86 @@ namespace Kid_Friendly_Web_Browser_Prototype
         {
             CategoryGridDockPanel.Children.Clear();
             CategoryGridDockPanel.Children.Add(new CategoryGrid1());
+            catGrid1 = true;
+            catGrid2 = false;
+            catGrid3 = false;
         }
 
         private void videos_Click(object sender, RoutedEventArgs e)
         {
             CategoryGridDockPanel.Children.Clear();
             CategoryGridDockPanel.Children.Add(new CategoryGrid2());
+            catGrid1 = false;
+            catGrid2 = true;
+            catGrid3 = false;
         }
 
         private void learn_Click(object sender, RoutedEventArgs e)
         {
             CategoryGridDockPanel.Children.Clear();
             CategoryGridDockPanel.Children.Add(new CategoryGrid3());
+            catGrid1 = false;
+            catGrid2 = false;
+            catGrid3 = true;
         }
 
         private void arrowRight_Click(object sender, RoutedEventArgs e)
         {
-            CategoryGrid1 catGrid1 = new CategoryGrid1();
-            CategoryGrid2 catGrid2 = new CategoryGrid2();
-            CategoryGrid3 catGrid3 = new CategoryGrid3();
-
-            if (CategoryGridDockPanel.Children.Equals(catGrid1))
+            if (catGrid1)
             {
                 CategoryGridDockPanel.Children.Clear();
                 CategoryGridDockPanel.Children.Add(new CategoryGrid2());
+                catGrid1 = false;
+                catGrid2 = true;
+                catGrid3 = false;
+            }
+
+            else if (catGrid2)
+            {
+                CategoryGridDockPanel.Children.Clear();
+                CategoryGridDockPanel.Children.Add(new CategoryGrid3());
+                catGrid1 = false;
+                catGrid2 = false;
+                catGrid3 = true;
+            }
+
+            else
+            {
+                CategoryGridDockPanel.Children.Clear();
+                CategoryGridDockPanel.Children.Add(new CategoryGrid1());
+                catGrid1 = true;
+                catGrid2 = false;
+                catGrid3 = false;
+            }
+        }
+
+        private void arrowLeft_Click(object sender, RoutedEventArgs e)
+        {
+            if (catGrid3)
+            {
+                CategoryGridDockPanel.Children.Clear();
+                CategoryGridDockPanel.Children.Add(new CategoryGrid2());
+                catGrid1 = false;
+                catGrid2 = true;
+                catGrid3 = false;
+            }
+
+            else if (catGrid2)
+            {
+                CategoryGridDockPanel.Children.Clear();
+                CategoryGridDockPanel.Children.Add(new CategoryGrid1());
+                catGrid1 = true;
+                catGrid2 = false;
+                catGrid3 = false;
+            }
+
+            else
+            {
+                CategoryGridDockPanel.Children.Clear();
+                CategoryGridDockPanel.Children.Add(new CategoryGrid3());
+                catGrid1 = false;
+                catGrid2 = false;
+                catGrid3 = true;
             }
         }
     }
